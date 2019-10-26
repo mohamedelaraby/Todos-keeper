c165264882de0500bb6df3f034a42a4da28b107a
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-10-26 14:44:00 +0200

    Delete and filter tasks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+
 // Define UI Vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
@@ -13,6 +14,15 @@ function loadEventListeners(){
 
    // Add task event
    form.addEventListener('submit', addTask);
+
+   // Remove task event
+   taskList.addEventListener('click', removeTask);
+
+   // Clear tasks event
+   clearBtn.addEventListener('click', clearTasks);
+
+   // Filter Tasks event
+   filter.addEventListener('keyup', filterTasks);
 }
 
 
@@ -56,3 +66,58 @@ function addTask(event){
    // Prevent the default behavior
    event.preventDefault();
 }
+
+
+
+// Remove task
+function removeTask(event){
+
+   // Check for the link 
+   if(event.target.parentElement.classList.contains('delete-item')){
+
+      // Remove the li itself
+      if(confirm('Are you sure?!')){
+         event.target.parentElement.parentElement.remove();
+      }
+   }
+}
+
+
+// Clear Tasks
+function clearTasks(){
+   if(taskList.hasChildNodes('.collection-item')){
+      //taskList.innerHTML = '';  // First way
+      
+   
+      // Faster way
+      while(taskList.firstChild){
+         taskList.removeChild(taskList.firstChild);
+      }
+
+   
+   } else {
+      alert('Add a task');
+   }
+ 
+}
+
+
+
+// Filter tasks
+function filterTasks(e){
+    // Get the filter input value
+    const text = e.target.value.toLowerCase();
+  
+    // Choose all list items
+    document.querySelectorAll('.collection-item').forEach( function(task){
+       // Get list text content
+       const item = task.firstChild.textContent;
+
+       if(item.toLowerCase().indexOf(text) !== -1){
+          task.style.display = 'block';
+       } else {
+          task.style.display = 'none';
+       }
+    })
+}
+
