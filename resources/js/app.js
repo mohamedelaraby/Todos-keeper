7feb7dc7ffb02e260a8c77121a09da9d7a2dd286
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-11-02 12:07:22 +0200

    Calculate and error functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,198 +1,89 @@
 
-// Define UI Vars
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
 
-// Load all event listeners
-loadEventListeners();
+/*
+    [  author ] ;- Muhammed alaraby
+    [  Date   ] ;- 19/10/19
+    [  Info   ] ;- Home page Script
+*/
 
-// load all event listeners
-function loadEventListeners(){
+// Define the UI
+ const loanForm      = document.querySelector('#loan-form'),
+       amountInput   = document.querySelector('#amount'),
+       interestInput = document.querySelector('#interest'),
+       yearsInput    = document.querySelector('#years');
 
-   // DOM Load event
-   document.addEventListener('DOMContentLoaded', getTasks);
+// Results UI
+const monthlyPayment = document.querySelector('#monthly-payment'),
+      totalPayment   = document.querySelector('#total-payment'),
+      totalInterest  = document.querySelector('#total-interest');   
+         
 
-   // Add task event
-   form.addEventListener('submit', addTask);
+// Load all Event listeners
+loadEventsListener();
 
-   // Remove task event
-   taskList.addEventListener('click', removeTask);
+function loadEventsListener(){
 
-   // Clear tasks event
-   clearBtn.addEventListener('click', clearTasks);
-
-   // Filter Tasks event
-   filter.addEventListener('keyup', filterTasks);
+   //Listen for submit 
+   loanForm.addEventListener('submit', calculateResults);
 }
 
+///////// [ Events Listeners functions ] ///////////////////////////////////
 
-// Get Tasks
-function getTasks(){
-   let tasks;
-   if(localStorage.getItem('tasks') === null){
-      tasks = [];
+// calculate the results
+function calculateResults(e){
+
+   // Calculations details
+   const principal = parseFloat(amount.value),
+         finalInterest = parseFloat(interestInput.value) / 100 / 12,
+         finalPayment = parseFloat(yearsInput.value) * 12;
+ 
+   // Compute monthly payment
+    const x = Math.pow( 1 + finalInterest, finalPayment);
+          monthly = (principal * x * finalInterest) / ( x - 1);      
+ 
+   
+   // Check if monthly is a finite number
+   if(isFinite(monthly)){
+      monthlyPayment.value = monthly.toFixed(2);
+      totalPayment.value = (monthly * finalPayment).toFixed(2);
+      totalInterest.value = ((monthly * finalPayment) - principal).toFixed(2);
    } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
+      showError(' Please check your numbers');
    }
-
-   // Append to the UI
-    tasks.forEach(function(task){
-  
-    const li = document.createElement('li');
-    li.className = 'collection-item';
-    li.appendChild(document.createTextNode(task));
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-times"></i>';
-    li.appendChild(link);
-    taskList.appendChild(li);
-
-   });
-}
-
-
-// Add Task ;- Operates adding new task process 
-function addTask(event){
-
-   if(taskInput.value === ''){
-      alert('Add  a Task');
-   }
-
-   // Create new list item for the new task
-   const li = document.createElement('li');
    
-   // Add class to list item
-   li.className = 'collection-item';
-
-   // Get the taskinput value
-   let taskTyped = taskInput.value;
-
-   // Create text node and append to li
-   li.appendChild(document.createTextNode(taskTyped));  
-
-   // Create new link element
-   const link = document.createElement('a');
    
-   // Add class
-   link.className = 'delete-item secondary-content';
-
-   // Add icon html tag
-   link.innerHTML = '<i class ="fa fa-times"></i>';
-
-   // Append link to the new task list
-   li.appendChild(link);
-
-   // Append the li to the ul
-  taskList.appendChild(li);
-
-  // Store in local storage
-  storeTasksInLocalStorage(taskInput.value);
-
-  // Clear the input
-  taskInput.value = '';
-
    // Prevent the default behavior
-   event.preventDefault();
-}
-
-// Store tasks in  local storage
-function storeTasksInLocalStorage(task){
-   
-   let tasks;
-   if (localStorage.getItem('tasks') === null) {
-      tasks = []; 
-      
-   } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-   }
-
-   // Put last added task into tasks local Storage
-   tasks.push(task);
-   
-   // Put into local storage
-   localStorage.setItem('tasks', JSON.stringify(tasks));
-
+   e.preventDefault();
 }
 
 
-// Remove task
-function removeTask(event){
+// Display errors
+function showError(error){
 
-   // Check for the link 
-   if(event.target.parentElement.classList.contains('delete-item')){
-
-      // Remove the li itself
-      if(confirm('Are you sure?!')){
-         event.target.parentElement.parentElement.remove();
-      
-          // Remove from LS
-          removeTaskFromLocalStorage(event.target.parentElement.parentElement)
-      }
-   }
-}
-
-// Remove from LS
-function removeTaskFromLocalStorage(taskItem){
-   let tasks;
-   if (localStorage.getItem('tasks') === null) {
-      tasks = []; 
-      
-   } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-   }
-
-   tasks.forEach(function(task,index){
-      if(taskItem.textContent == task){
-         tasks.splice(index,1);
-      }
-   });
-
-   localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+    // Get error position element
+    const card = document.querySelector('.card'),
+          heading = document.querySelector('.heading');
 
 
-// Clear Tasks
-function clearTasks(){
-   if(taskList.hasChildNodes('.collection-item')){
-      //taskList.innerHTML = '';  // First way
-      // Faster way
-      while(taskList.firstChild){
-         taskList.removeChild(taskList.firstChild);
-      }
-   } else {
-      alert('Add a task');
-   }
+ // Create a div
+ const errorDiv = document.createElement('div');
 
-   // Clear from Local Storage
-   clearTasksFromlocalStorage();
-}
+ // Add a class
+ errorDiv.className = ' alert alert-danger';
 
+ // create  text node and append to div
+ errorDiv.appendChild(document.createTextNode(error));
 
-// Clear Tasks From Local Storage
-function clearTasksFromlocalStorage(){
-localStorage.clear();
-}
+ // Insert error above heading
+ card.insertBefore(errorDiv, heading);
 
+ // Clear error after 3 seconds
+ setTimeout(clearError, 3000);
 
-
-// Filter tasks
-function filterTasks(e){
-    // Get the filter input value
-    const text = e.target.value.toLowerCase();
   
-    // Choose all list items
-    document.querySelectorAll('.collection-item').forEach( function(task){
-       // Get list text content
-       const item = task.firstChild.textContent;
-
-       if(item.toLowerCase().indexOf(text) !== -1){
-          task.style.display = 'block';
-       } else {
-          task.style.display = 'none';
-       }
-    })
 }
 
+// clear Error
+function clearError(){
+   document.querySelector('.alert').remove();
+}
